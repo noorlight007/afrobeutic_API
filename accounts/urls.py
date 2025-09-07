a7376a14a734b69aae554f57f4ab2bf5654c0a2c
noorlight07 <contactwnoor@gmail.com>
2025-09-08 05:16:11 +0600

    updated in local

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,16 @@
 # accounts/urls.py
 from django.urls import path
-from .views import ListOfAccountsView, RegisterView, VerifyView, LoginView
+from .views import ListOfAccountsView, RegisterView, VerifyView, LoginView, AdminRegisterView, AdminVerifyView
 from .views_token import RefreshTokenView, TokenVerifyView
 
 app_name = "accounts"
 
 urlpatterns = [
     path("auth/register", RegisterView.as_view(), name="register"),
+    path("auth/register/admin", AdminRegisterView.as_view(), name="admin_register"),
     path("auth/login", LoginView.as_view(), name="login"),
     path("auth/verify",   VerifyView.as_view(),   name="verify"),
+    path("auth/verify/admin",   AdminVerifyView.as_view(),   name="admin_verify"),
     path("list/accounts",   ListOfAccountsView.as_view(),   name="list_accounts"),
 
     path("auth/token/refresh", RefreshTokenView.as_view(), name="auth_token_refresh"),
